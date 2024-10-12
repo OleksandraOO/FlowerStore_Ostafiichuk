6e32f8b3aea137a2ae968bb6e7c4073164471e91
Oleksandra <ostafiichuk.pn@ucu.edu.ua>
2024-10-12 21:45:38 +0300

    linter fix

--- src/test/java/flower/store/FlowerPackTest.java
+++ src/test/java/flower/store/FlowerPackTest.java
@@ -8,9 +8,9 @@ public class FlowerPackTest {
     private Flower flower;
     private FlowerPack flowerPack;
 
-    private final double VAL = 10;
-    private final double VAL1 = 5;
-    private final double DELTA = 0.001;
+    private final double val1 = 10;
+    private final double val2 = 5;
+    private final double delta = 0.001;
     
 
 
@@ -18,32 +18,32 @@ public class FlowerPackTest {
     public void init() {
         // Create a flower object with specific attributes
         flower = new Flower();
-        flower.setPrice(VAL);  // Set price per flower
-        flower.setSepalLength(VAL1);
+        flower.setPrice(val1);  // Set price per flower
+        flower.setSepalLength(val2);
         flower.setFlowerType(FlowerType.ROSE);
         flower.setColor(FlowerColor.RED);
 
         // Create a flower pack with 5 flowers
-        flowerPack = new FlowerPack(flower, (int)VAL1);
+        flowerPack = new FlowerPack(flower, (int) val2);
     }
 
     @Test
     public void testGetPrice() {
         // Test if the price is calculated correctly
-        double expectedPrice = VAL * VAL1;  // Price per flower * amount
+        double expectedPrice = val1 * val2;  // Price per flower * amount
         assertEquals(expectedPrice, flowerPack.getPrice(),
-         DELTA, "Price calculation failed");
+         delta, "Price calculation failed");
     }
 
     @Test
     public void testSetAmount() {
         // Change the amount of flowers in the pack
-        flowerPack.setAmount((int)VAL);
+        flowerPack.setAmount((int) val1);
 
         // Test if the new price reflects the updated amount
-        double expectedPrice = VAL * VAL;  // Price per flower * new amount
+        double expectedPrice = val1 * val1;  // Price per flower * new amount
         assertEquals(expectedPrice, flowerPack.getPrice(),
-         DELTA,
+         delta,
           "Updated price calculation failed");
     }
 }
